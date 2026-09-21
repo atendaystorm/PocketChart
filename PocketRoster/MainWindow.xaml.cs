5d729775081f78331af954323df01db493d5a4d2
atendaystorm <tendaystorm@gmail.com>
2026-09-20 23:49:29 -0500

    Add project files.

---
+++ PocketRoster/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+
+namespace PocketChartDesktop
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            await PocketChartBrowser.EnsureCoreWebView2Async();
+
+            PocketChartBrowser.CoreWebView2.Navigate(
+                "https://pocketchart.onrender.com/"
+            );
+        }
+    }
+}
